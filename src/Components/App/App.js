49bbb69c29b893a44fdf7f3feae747f0862edd52
Brian Nwaeke <bnwaeke@icloud.com>
2022-01-09 03:31:24 -0800

    build track, search, playlist, tracklist component

---
+++ src/Components/App/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+
+
+function App() {
+    return (
+      <div>
+        <h1>Not<span className="highlight">Spot</span>ify</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults /> 
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export default App;
