9f55b5215c1ef5e1fa9cdecbaae5a900407e785f
Brian Nwaeke <bnwaeke@icloud.com>
2022-01-10 18:02:22 -0800

    Change the Name of a Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -36,6 +37,10 @@ class App extends React.Component {
     tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
     this.setState({ playlistTracks: tracks });
   }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
   
   render() {
     return (
@@ -45,7 +50,11 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} /> 
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+            <Playlist playlistName={this.state.playlistName} 
+                  playlistTracks={this.state.playlistTracks} 
+                  onRemove={this.removeTrack} 
+                  onNameChange={this.updatePlaylistName}
+                  />
           </div>
         </div>
       </div>
